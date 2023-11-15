01a49a22fa9db10f7eb48f147c8a2356f82678cb
Andrés Silva <asilvam133@gmail.com>
2023-11-15 15:53:29 -0500

    Add Scala support

---
+++ lua/after/scala.lua
@@ -0,0 +1,65 @@
+local util = require('util')
+
+-- scala
+local api = vim.api
+-- options
+vim.opt_global.completeopt = { "menuone", "noinsert", "noselect" }
+vim.opt_global.shortmess:remove('F')
+vim.opt_global.shortmess:remove('c')
+
+-- LSP setup
+local metals_config = require('metals').bare_config()
+
+-- settings
+metals_config.settings = {
+  showImplicitArguments = true,
+  showInferredType = true,
+  superMethodLensesEnabled = true,
+  showImplicitConversionsAndClasses = true,
+}
+
+metals_config.init_options.statusBarProvider = 'on'
+
+metals_config.capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+-- DAP
+local dap = require('dap')
+
+dap.configurations.scala = {
+  {
+    type = 'scala',
+    request = 'launch',
+    name = 'RunOrTest',
+    metals = {
+      runType = 'runOrTestFile',
+    },
+  },
+  {
+    type = 'scala',
+    request = 'launch',
+    name = 'Test Target',
+    metals = {
+      runType = 'testTarget',
+    },
+  },
+}
+
+metals_config.on_attach = function(client, bufnr)
+  util.on_attach(client, bufnr)
+  require('metals').setup_dap()
+end
+
+-- autocmd to start the server with all the configurations above
+local nvim_metals_group = api.nvim_create_augroup('nvim-metals', {clear = true,})
+
+api.nvim_create_autocmd('FileType', {
+  pattern = { 'scala', 'sbt', 'java', },
+  callback = function()
+    require('metals').initialize_or_attach(metals_config)
+  end,
+  group = nvim_metals_group,
+})
+
+vim.keymap.set('n', '<leader>cs<space>', require('telescope').extensions.metals.commands, {desc = 'Commands'})
+vim.keymap.set('n', '<leader>csa', require('metals').new_scala_file, {desc = 'New Scala file'})
+vim.keymap.set('n', '<leader>csr', require('metals').restart_metals, {desc = 'Restart Metals'})
