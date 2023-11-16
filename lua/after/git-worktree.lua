256769625076a9cbefbfc38636d725fa50aa159f
Andrés Silva <asilvam133@gmail.com>
2023-11-16 09:57:00 -0500

    Change keymap to support Neogit

--- lua/after/git-worktree.lua
+++ lua/after/git-worktree.lua
@@ -3,4 +3,4 @@ require('telescope').load_extension('git_worktree')
 
 -- keymaps
 vim.keymap.set('n', '<leader>gw', require('telescope').extensions.git_worktree.git_worktrees, { desc = '[G]it [W]orktrees', silent = true, })
-vim.keymap.set('n', '<leader>gc', require('telescope').extensions.git_worktree.create_git_worktree, { desc = '[G]it [C]reate Worktree', silent = true, })
+vim.keymap.set('n', '<leader>ga', require('telescope').extensions.git_worktree.create_git_worktree, { desc = '[G]it [C]reate Worktree', silent = true, })
