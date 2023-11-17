8e648c907059d07cd2c682d5a1110d24677afca1
Andrés Silva <asilvam133@gmail.com>
2023-11-17 16:20:35 -0500

    Set Treesitter-Context be default

--- lua/after/treesitter-context.lua
+++ lua/after/treesitter-context.lua
@@ -2,4 +2,7 @@ require('treesitter-context').setup {
   max_lines = 3,
 }
 
+-- Enable context by default
+require('treesitter-context').enable()
+
 vim.keymap.set('n', '<leader>uc', require('treesitter-context').toggle, { desc = 'Toggle Treesitter-[C]ontext '})
