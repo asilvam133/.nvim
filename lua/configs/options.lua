8380df67b5bc4ad16be9d68465a3e68a34e50014
Andrés Silva <asilvam133@gmail.com>
2023-11-17 19:16:45 -0500

    Change LSP diagnostics icons

--- lua/configs/options.lua
+++ lua/configs/options.lua
@@ -45,3 +45,10 @@ vim.o.termguicolors = true
 
 -- Remove 'fillchars'
 vim.api.nvim_command('set fillchars=eob:\\ ')
+
+-- Change diagnostics icons
+local signs = { Error = "󰅚 ", Warn = "󰀪 ", Hint = "󰌶 ", Info = " " }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
