06e1eea5ea4a7d97c83ead94278437bbbd6f7665
Andrés Silva <asilvam133@gmail.com>
2023-11-19 12:34:10 -0500

    Set Which-key to be lazy load

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,5 +1,20 @@
 return {
-    -- Useful plugin to show you pending keybinds.
-  { 'folke/which-key.nvim',  opts = {} },
-
+  'folke/which-key.nvim',
+  event = 'VeryLazy',
+  opts = {},
+  config = function()
+    require('which-key').register {
+      ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
+      ['<leader>cr'] = { name = '[R]ust', _ = 'which_key_ignore' },
+      ['<leader>cs'] = { name = '[S]cala', _ = 'which_key_ignore' },
+      ['<leader>d'] = { name = '[D]ebug', _ = 'which_key_ignore' },
+      ['<leader>f'] = { name = '[F]ile', _ = 'which_key_ignore' },
+      ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
+      ['<leader>h'] = { name = 'More git', _ = 'which_key_ignore' },
+      ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
+      ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
+      ['<leader>u'] = { name = '[U]I', _ = 'which_key_ignore' },
+      ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+    }
+  end,
 }
