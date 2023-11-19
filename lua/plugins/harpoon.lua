020e53e88cf8d9a1dd83529a104e65b48c032a1a
Andrés Silva <asilvam133@gmail.com>
2023-11-19 11:59:04 -0500

    Set Harpoon to lazy load

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,9 +1,14 @@
 return {
-    {
-    'ThePrimeagen/harpoon',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-    },
+  'ThePrimeagen/harpoon',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
   },
-
+  keys = {
+    { '<C-a>', function() require('harpoon.mark').add_file() end,        mode = 'n', desc = 'Harpoon: [A]dd file' },
+    { '<C-e>', function() require('harpoon.ui').toggle_quick_menu() end, mode = 'n', desc = 'Harpoon: [E]xplore files' },
+    { '<C-h>', function() require('harpoon.ui').nav_file(1) end,         mode = 'n', desc = 'Harpoon: [1] file' },
+    { '<C-t>', function() require('harpoon.ui').nav_file(2) end,         mode = 'n', desc = 'Harpoon: [2] file' },
+    { '<C-n>', function() require('harpoon.ui').nav_file(3) end,         mode = 'n', desc = 'Harpoon: [3] file' },
+    { '<C-s>', function() require('harpoon.ui').nav_file(4) end,         mode = 'n', desc = 'Harpoon: [4] file' },
+  }
 }
