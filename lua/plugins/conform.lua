1f9273ae41bf08502b9cdbd245fb4eed9919583e
Andrés Silva <asilvam133@gmail.com>
2023-11-19 11:26:10 -0500

    Set Conform to lazy load

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -2,5 +2,22 @@ return {
   'stevearc/conform.nvim',
   event = { 'BufWritePre' },
   cmd = { 'ConformInfo' },
-  opts = {},
+  opts = {
+    format = {
+      timeout_ms = 3000,
+      async = false,
+      quiet = false,
+    },
+    formatters_by_ft = {
+      lua = { 'stylua' },
+      sh = { 'shfmt' },
+      yaml = { 'yamlfmt' }
+    },
+    formatters = {
+      injected = { options = { ignore_errors = true } },
+    },
+  },
+  keys = {
+    { '<leader>cc', function() require('conform').format({ async = true, lsp_fallback = true }) end, mode = 'n', desc = '[C]onform: [C]ode Format' },
+  }
 }
