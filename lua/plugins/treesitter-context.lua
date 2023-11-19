c44d4c326656fcdd7e3d9f66daee8d6ec06360d2
Andrés Silva <asilvam133@gmail.com>
2023-11-19 12:54:03 -0500

    Set Treesitter-context to be lazy load

--- lua/plugins/treesitter-context.lua
+++ lua/plugins/treesitter-context.lua
@@ -1,6 +1,10 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter-context",
-    enabled = true,
+  "nvim-treesitter/nvim-treesitter-context",
+  enabled = true,
+  opts = {
+    max_lines = 3,
+  },
+  keys = {
+    { '<leader>uc', function() require('treesitter-context').toggle() end, mode = 'n', desc = 'Toggle Treesitter-[C]ontext ' },
   }
 }
