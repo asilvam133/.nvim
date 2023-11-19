ad729094ee89568fca8ea1aac03add28b9049bf2
Andrés Silva <asilvam133@gmail.com>
2023-11-19 11:23:57 -0500

    Set lazy loading the default

--- init.lua
+++ init.lua
@@ -25,6 +25,9 @@ require('lazy').setup({
     { import = 'plugins' },
     { import = 'plugins.languages' },
   },
+  defaults = {
+    lazy = true,
+  },
 }, {})
 
 -- Load plugins extended configurations
