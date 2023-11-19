abb259660b013a2d1a8eaa1704cef9a2c4d6a54c
Andrés Silva <asilvam133@gmail.com>
2023-11-19 12:54:03 -0500

    Set Treesitter-context to be lazy load

--- lua/after/treesitter-context.lua
+++
@@ -1,8 +0,0 @@
-require('treesitter-context').setup {
-  max_lines = 3,
-}
-
--- Enable context by default
-require('treesitter-context').enable()
-
-vim.keymap.set('n', '<leader>uc', require('treesitter-context').toggle, { desc = 'Toggle Treesitter-[C]ontext '})
