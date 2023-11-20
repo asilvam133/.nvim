fcb5ab7359d746ad97cdb37e5d91f7c126549ea0
Andrés Silva <asilvam133@gmail.com>
2023-11-20 06:00:31 -0500

    Drop 'After' config since all plugins are lazy

--- init.lua
+++ init.lua
@@ -30,8 +30,5 @@ require('lazy').setup({
   },
 }, {})
 
--- Load plugins extended configurations
-require('after')
-
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
