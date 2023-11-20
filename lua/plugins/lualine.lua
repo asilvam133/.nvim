1d04808de1eaf601a588974988027cd0207c8d5b
Andrés Silva <asilvam133@gmail.com>
2023-11-20 05:13:27 -0500

    Set Lualine to load after Lazy is done

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,24 +1,21 @@
 local util = require("util")
 
 return {
-  {
-    -- Set lualine as statusline
-    'nvim-lualine/lualine.nvim',
-    dependencies = {
-      'nvim-tree/nvim-web-devicons',
-      lazy = true,
+  'nvim-lualine/lualine.nvim',
+  event = 'VeryLazy',
+  dependencies = {
+    'nvim-tree/nvim-web-devicons',
+    lazy = true,
+  },
+  opts = {
+    options = {
+      icons_enabled = true,
+      theme = 'rose-pine',
+      component_separators = '|',
+      section_separators = '',
     },
-    opts = {
-      options = {
-        icons_enabled = true,
-        theme = 'rose-pine',
-        component_separators = '|',
-        section_separators = '',
-      },
-      sections = {
-        lualine_b = { util.clients_lsp, 'branch', 'diff', 'diagnostics' },
-      },
+    sections = {
+      lualine_b = { util.clients_lsp, 'branch', 'diff', 'diagnostics' },
     },
   },
-
 }
