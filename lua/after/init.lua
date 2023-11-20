aa04dc43d3c95a4e37064452778c9c1c71b3ad98
Andrés Silva <asilvam133@gmail.com>
2023-11-20 04:41:14 -0500

    Set Mason and LSP to lazy load

--- lua/after/init.lua
+++ lua/after/init.lua
@@ -1,5 +1,4 @@
 -- Here it goes the specific colorscheme. It's best to change specific things about the scheme
 require('after.telescope')
-require('after.mason')
 require('after.rust-tools')
 require('after.scala')
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,21 +1,51 @@
 return {
-  -- NOTE: This is where your plugins related to LSP can be installed.
-  --  The configuration is done below. Search for lspconfig to find it below.
-  {
-    -- LSP Configuration & Plugins
-    'neovim/nvim-lspconfig',
-    dependencies = {
-      -- Automatically install LSPs to stdpath for neovim
-      'williamboman/mason.nvim',
-      'williamboman/mason-lspconfig.nvim',
-
-      -- Useful status updates for LSP
-      -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-      { 'j-hui/fidget.nvim', tag = 'legacy', opts = {} },
-
-      -- Additional lua configuration, makes nvim stuff amazing!
-      'folke/neodev.nvim',
+  'neovim/nvim-lspconfig',
+  lazy = false,
+  dependencies = {
+    'williamboman/mason.nvim',
+    'williamboman/mason-lspconfig.nvim',
+    { 'j-hui/fidget.nvim', tag = 'legacy', opts = {} },
+    'folke/neodev.nvim',
+  },
+  opts = {
+    servers = {
+      lua_ls = {
+        Lua = {
+          workspace = { checkThirdParty = false },
+          telemetry = { enable = false },
+          diagnostics = {
+            globals = { 'vim' },
+          },
+        },
+      },
+      yamlls = {},
     },
   },
+  config = function(_, opts)
+    local util = require('util')
 
+    require('mason').setup()
+    require('mason-lspconfig').setup()
+
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+
+    local mason_lspconfig = require('mason-lspconfig')
+
+    local servers = opts.servers
+    mason_lspconfig.setup({
+      ensure_installed = vim.tbl_keys(servers),
+    })
+
+    mason_lspconfig.setup_handlers({
+      function(server_name)
+        require('lspconfig')[server_name].setup({
+          capabilities = capabilities,
+          on_attach = util.on_attach,
+          settings = servers[server_name],
+          filetypes = (servers[server_name] or {}).filetypes,
+        })
+      end,
+    })
+  end,
 }
