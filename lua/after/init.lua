baf4700aafdab664822ac285b1d7e7850b0b22d1
Andrés Silva <asilvam133@gmail.com>
2023-11-19 19:21:51 -0500

    Set Treesitter to be lazy load

--- lua/after/init.lua
+++ lua/after/init.lua
@@ -1,6 +1,5 @@
 -- Here it goes the specific colorscheme. It's best to change specific things about the scheme
 require('after.telescope')
-require('after.treesitter')
 require('after.mason')
 require('after.rust-tools')
 require('after.scala')
