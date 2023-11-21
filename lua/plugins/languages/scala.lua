053a17b8ef685242511e4c7cb5615a4611893515
Andrés Silva <asilvam133@gmail.com>
2023-11-21 07:11:17 -0500

    Fix Scala CMP

--- lua/plugins/languages/scala.lua
+++ lua/plugins/languages/scala.lua
@@ -1,69 +1,60 @@
 return {
   {
+    "hrsh7th/nvim-cmp",
+    requires = {
+      { "hrsh7th/cmp-nvim-lsp" },
+      { "hrsh7th/cmp-vsnip" },
+      { "hrsh7th/vim-vsnip" },
+    },
+  },
+  {
     'scalameta/nvim-metals',
     dependencies = { 'nvim-lua/plenary.nvim', 'mfussenegger/nvim-dap' },
     init = function()
-      local util = require('util')
-
-      -- scala
-      local api = vim.api
-      -- options
-      vim.opt_global.completeopt = { 'menuone', 'noinsert', 'noselect' }
-      vim.opt_global.shortmess:remove('F')
-      vim.opt_global.shortmess:remove('c')
-
-      -- LSP setup
-      local metals_config = require('metals').bare_config()
-
-      -- settings
+      local metals_config = require("metals").bare_config()
+      metals_config.init_options.statusBarProvider = "on"
       metals_config.settings = {
         showImplicitArguments = true,
-        showInferredType = true,
-        superMethodLensesEnabled = true,
-        showImplicitConversionsAndClasses = true,
+        excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
       }
-
-      metals_config.init_options.statusBarProvider = 'on'
-
-      metals_config.capabilities = require('cmp_nvim_lsp').default_capabilities()
-
-      -- DAP
-      local dap = require('dap')
-
-      dap.configurations.scala = {
-        {
-          type = 'scala',
-          request = 'launch',
-          name = 'RunOrTest',
-          metals = {
-            runType = 'runOrTestFile',
-          },
-        },
-        {
-          type = 'scala',
-          request = 'launch',
-          name = 'Test Target',
-          metals = {
-            runType = 'testTarget',
-          },
-        },
-      }
-
-      metals_config.on_attach = function(client, bufnr)
-        util.on_attach(client, bufnr)
-        require('metals').setup_dap()
+      metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
+      metals_config.on_attach = function(_, _)
+        require("metals").setup_dap()
       end
 
-      -- autocmd to start the server with all the configurations above
-      local nvim_metals_group = api.nvim_create_augroup('nvim-metals', { clear = true })
-
-      api.nvim_create_autocmd('FileType', {
-        pattern = { 'scala', 'sbt', 'java' },
+      local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+      vim.api.nvim_create_autocmd("FileType", {
+        -- NOTE: You may or may not want java included here. You will need it if you
+        -- want basic Java support but it may also conflict if you are using
+        -- something like nvim-jdtls which also works on a java filetype autocmd.
+        pattern = { "scala", "sbt", "java" },
         callback = function()
-          require('metals').initialize_or_attach(metals_config)
+          require("metals").initialize_or_attach(metals_config)
         end,
         group = nvim_metals_group,
       })
+
+      -- Debug settings
+      local dap = require("dap")
+      dap.configurations.scala = {
+        {
+          type = "scala",
+          request = "launch",
+          name = "RunOrTest",
+          metals = {
+            runType = "runOrTestFile",
+            --args = { "firstArg", "secondArg", "thirdArg" }, -- here just as an example
+          },
+        },
+        {
+          type = "scala",
+          request = "launch",
+          name = "Test Target",
+          metals = {
+            runType = "testTarget",
+          },
+        },
+      }
       vim.keymap.set('n', '<leader>csa', function() require('metals').new_scala_file() end, { desc = 'New Scala file' })
       vim.keymap.set('n', '<leader>csr', function() require('metals').restart_metals() end, { desc = 'Restart Metals' })
     end,
