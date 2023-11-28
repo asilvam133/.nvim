04622941d1a779a838bd5413a9f3757b2dea0b1d
Andrés Silva <asilvam133@gmail.com>
2023-11-27 19:48:04 -0500

    Set rose-pine gold to be lighter

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,17 +4,40 @@ return {
     name = 'rose-pine',
     lazy = false,
     priority = 1000,
-    opts = {
-      disable_background = true,
-    },
-    config = function()
-      local p = require('rose-pine.palette')
-
+    opts = function()
+      local lighter_gold = "#DFBCAB"
+      return {
+        disable_background = true,
+        highlight_groups = {
+          Normal = { bg = 'none' },
+          NormalFloat = { bg = 'none' },
+          Question = { fg = lighter_gold },
+          RedrawDebugClear = { fg = lighter_gold },
+          WarningMsg = { fg = lighter_gold },
+          Character = { fg = lighter_gold },
+          Constant = { fg = lighter_gold },
+          Float = { fg = lighter_gold },
+          Number = { fg = lighter_gold },
+          String = { fg = lighter_gold },
+          BufferCurrentTarget = { fg = lighter_gold },
+          BufferInactiveTarget = { fg = lighter_gold },
+          BufferVisibleTarget = { fg = lighter_gold },
+          ModesCopy = { fg = lighter_gold },
+          WhichKeyDesc = { fg = lighter_gold },
+          CmpItemKindSnippet = { fg = lighter_gold },
+          DapUIThread = { fg = lighter_gold },
+          DapUIBreakpointsCurrentLine = { fg = lighter_gold },
+          TelescopeBorder =  { fg = 'highlight_high', bg = 'none'},
+          TelescopeNormal =  { bg = 'none'},
+          TelescopePromptNormal =  { bg = 'base'},
+          TelescopeResultNormal =  { fg = 'subtle', bg = 'none'},
+          TelescopeSelection =  { fg = 'text', bg = 'base'},
+        },
+      }
+    end,
+    config = function(_, opts)
+      require('rose-pine').setup(opts)
       vim.cmd.colorscheme('rose-pine')
-      vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
-      vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
-      vim.api.nvim_set_hl(0, 'String', { fg = p.rose })
-      vim.api.nvim_set_hl(0, 'Constant', { fg = p.rose })
     end,
   },
 }
