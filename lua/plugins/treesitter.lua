6f7d6b9c8417dcfa8d339b46b7bea383254ce28c
Andrés Silva <asilvam133@gmail.com>
2023-11-28 22:56:06 -0500

    Set Treesitter to load when Lazy is done

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,6 +5,7 @@ return {
     'nvim-treesitter/nvim-treesitter-textobjects',
   },
   build = ':TSUpdate',
+  event = 'VeryLazy',
   config = function()
     require('nvim-treesitter.configs').setup {
       -- Add languages to be installed here that you want installed for treesitter
