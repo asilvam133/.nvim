f7a003670450c035cdd6077a0364bf19e3e60b99
Andrés Silva <asilvam133@gmail.com>
2023-11-30 10:04:37 -0500

    Catppuccin for the win!

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -11,9 +11,22 @@ return {
         NormalFloat = { bg = 'none' },
       }
     },
-    config = function(_, opts)
-      require('rose-pine').setup(opts)
-      vim.cmd.colorscheme('rose-pine')
-    end,
+    -- config = function(_, opts)
+    --   require('rose-pine').setup(opts)
+    --   vim.cmd.colorscheme('rose-pine')
+    -- end,
   },
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    lazy = false,
+    priority = 1000,
+    opts = {
+      transparent_background = true,
+    },
+    config = function(_, opts)
+      require('catppuccin').setup(opts)
+      vim.cmd.colorscheme('catppuccin-mocha')
+    end,
+  }
 }
