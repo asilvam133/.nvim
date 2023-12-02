93fcb11fcde26b2d2510830dbbff4bb6745e7c4f
Andrés Silva <asilvam133@gmail.com>
2023-12-02 17:44:09 -0500

    Delete unused which-key group

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -8,7 +8,6 @@ return {
       ['<leader>cr'] = { name = '[R]ust', _ = 'which_key_ignore' },
       ['<leader>f'] = { name = '[F]ile', _ = 'which_key_ignore' },
       ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-      ['<leader>h'] = { name = 'More git', _ = 'which_key_ignore' },
       ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
       ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
       ['<leader>u'] = { name = '[U]I', _ = 'which_key_ignore' },
