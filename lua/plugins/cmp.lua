411e6dd8911fb669ed7a5ec92cf55721d2e356d0
Andrés Silva <asilvam133@gmail.com>
2023-12-03 12:50:51 -0500

    Set icons for CMP snippets

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -4,15 +4,14 @@ return {
   version = false, -- last release is way too old
   event = "InsertEnter",
   dependencies = {
-    -- Snippet Engine & its associated nvim-cmp source
-    'L3MON4D3/LuaSnip',
+    {
+      -- snippet plugin
+      "L3MON4D3/LuaSnip",
+      dependencies = "rafamadriz/friendly-snippets",
+      opts = { history = true, updateevents = "TextChanged,TextChangedI" },
+    },
     'saadparwaiz1/cmp_luasnip',
-
-    -- Adds LSP completion capabilities
     'hrsh7th/cmp-nvim-lsp',
-
-    -- Adds a number of user-friendly snippets
-    'rafamadriz/friendly-snippets',
   },
   opts = function()
     local cmp = require('cmp')
@@ -21,15 +20,106 @@ return {
     require('luasnip.loaders.from_vscode').lazy_load()
     luasnip.config.setup {}
 
+    local field_arrangement = {
+      atom = { "kind", "abbr", "menu" },
+      atom_colored = { "kind", "abbr", "menu" },
+    }
+
+    local icons = {
+      Namespace = "󰌗",
+      Text = "󰉿",
+      Method = "󰆧",
+      Function = "󰆧",
+      Constructor = "",
+      Field = "󰜢",
+      Variable = "󰀫",
+      Class = "󰠱",
+      Interface = "",
+      Module = "",
+      Property = "󰜢",
+      Unit = "󰑭",
+      Value = "󰎠",
+      Enum = "",
+      Keyword = "󰌋",
+      Snippet = "",
+      Color = "󰏘",
+      File = "󰈚",
+      Reference = "󰈇",
+      Folder = "󰉋",
+      EnumMember = "",
+      Constant = "󰏿",
+      Struct = "󰙅",
+      Event = "",
+      Operator = "󰆕",
+      TypeParameter = "󰊄",
+      Table = "",
+      Object = "󰅩",
+      Tag = "",
+      Array = "[]",
+      Boolean = "",
+      Number = "",
+      Null = "󰟢",
+      String = "󰉿",
+      Calendar = "",
+      Watch = "󰥔",
+      Package = "",
+      Copilot = "",
+      Codeium = "",
+      TabNine = "",
+    }
+
+    local cmp_icons = true
+    local lspkind_text = true
+    local cmp_style = "default"
+
+    local formatting_style = {
+      -- default fields order i.e completion word + item.kind + item.kind icons
+      fields = field_arrangement[cmp_style] or { "abbr", "kind", "menu" },
+
+      format = function(_, item)
+        local icon = (cmp_icons and icons[item.kind]) or ""
+
+        if cmp_style == "atom" or cmp_style == "atom_colored" then
+          icon = " " .. icon .. " "
+          item.menu = lspkind_text and "   (" .. item.kind .. ")" or ""
+          item.kind = icon
+        else
+          icon = lspkind_text and (" " .. icon .. " ") or icon
+          item.kind = string.format("%s %s", icon, lspkind_text and item.kind or "")
+        end
+        return item
+      end,
+    }
+
+    local function border(hl_name)
+      return {
+        { "╭", hl_name },
+        { "─", hl_name },
+        { "╮", hl_name },
+        { "│", hl_name },
+        { "╯", hl_name },
+        { "─", hl_name },
+        { "╰", hl_name },
+        { "│", hl_name },
+      }
+    end
+
     return {
       completion = {
         completeopt = "menu,menuone,noinsert",
+      },
+      window = {
+        documentation = {
+          border = border "CmpDocBorder",
+          winhighlight = "Normal:CmpDoc",
+        },
       },
       snippet = {
         expand = function(args)
           require("luasnip").lsp_expand(args.body)
         end,
       },
+      formatting = formatting_style,
       mapping = cmp.mapping.preset.insert({
         ['<C-n>'] = cmp.mapping.select_next_item(),
         ['<C-p>'] = cmp.mapping.select_prev_item(),
@@ -56,11 +146,11 @@ return {
       sorting = defaults.sorting,
     }
   end,
----@param opts cmp.ConfigSchema
-    config = function(_, opts)
-      for _, source in ipairs(opts.sources) do
-        source.group_index = source.group_index or 1
-      end
-      require("cmp").setup(opts)
-    end,
+  ---@param opts cmp.ConfigSchema
+  config = function(_, opts)
+    for _, source in ipairs(opts.sources) do
+      source.group_index = source.group_index or 1
+    end
+    require("cmp").setup(opts)
+  end,
 }
