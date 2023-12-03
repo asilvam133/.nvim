67e6e86b952d56a9c00712ff5c561f2532ff1917
Andrés Silva <asilvam133@gmail.com>
2023-12-02 20:00:21 -0500

    Add diagnostics and DAP better icons

--- lua/configs/options.lua
+++ lua/configs/options.lua
@@ -52,8 +52,23 @@ vim.o.termguicolors = true
 vim.api.nvim_command('set fillchars=eob:\\ ')
 
 -- Change diagnostics icons
-local signs = { Error = "󰅚 ", Warn = "󰀪 ", Hint = "󰌶 ", Info = " " }
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " ", }
 for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
+
+local dap_signs = {
+  Stopped             = { "󰁕 ", "DiagnosticWarn", "DapStoppedLine" },
+  Breakpoint          = " ",
+  BreakpointCondition = " ",
+  BreakpointRejected  = { " ", "DiagnosticError" },
+  LogPoint            = ".>",
+}
+for name, sign in pairs(dap_signs) do
+  sign = type(sign) == "table" and sign or { sign }
+  vim.fn.sign_define(
+    "Dap" .. name,
+    { text = sign[1], texthl = sign[2] or "DiagnosticInfo", linehl = sign[3], numhl = sign[3] }
+  )
 end
