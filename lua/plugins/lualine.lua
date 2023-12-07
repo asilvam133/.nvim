3684002a46fc03e9221cbcad2c352bb6c11d42d8
Andrés Silva <asilvam133@gmail.com>
2023-12-06 21:28:49 -0500

    Let's get real with Catppuccin-mocha

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -11,9 +11,28 @@ return {
     options = {
       icons_enabled = true,
       theme = 'auto',
+      component_separators = { " ", " " },
+      section_separators = { left = "", right = "" },
     },
     sections = {
       lualine_b = { util.clients_lsp, 'branch', 'diff', 'diagnostics' },
+      lualine_c = {
+        function()
+          return "%="
+        end,
+        {
+          "filename",
+          file_status = true,
+          path = 1,
+          shorting_target = 40,
+          symbols = {
+            modified = "󰐖",
+            readonly = "",
+            unnamed = "[No Name]",
+            newfile = "[New File]",
+          },
+        }
+      },
     },
   },
 }
