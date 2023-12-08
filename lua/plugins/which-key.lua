524267aa2c01f4922f90cd12a5e33ada1a4d5641
Andrés Silva <asilvam133@gmail.com>
2023-12-08 18:02:31 -0500

    Add Quickfix list keybindings

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -10,8 +10,8 @@ return {
       ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
       ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
       ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
+      ['<leader>q'] = { name = '[Q]uickfix', _ = 'which_key_ignore' },
       ['<leader>u'] = { name = '[U]I', _ = 'which_key_ignore' },
-      ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
     }
   },
   config = function(_, opts)
