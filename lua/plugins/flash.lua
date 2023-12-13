2197da79beb619cabfd1f601fa23447e221e4ced
Andrés Silva <asilvam133@gmail.com>
2023-12-13 15:06:57 -0500

    Add Flash

---
+++ lua/plugins/flash.lua
@@ -0,0 +1,22 @@
+return {
+    {
+        'folke/flash.nvim',
+        event = 'VeryLazy',
+        opts = {},
+        keys = {
+            { "<leader>s<space>", mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Jump" },
+            { "<leader>ss",       mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Jump treesitter" },
+            { "<leader>sr",       mode = "o",               function() require("flash").remote() end,            desc = "Search remote" },
+            { "<leader>sR",       mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Search treesitter" },
+        },
+    },
+    {
+        'folke/which-key.nvim',
+        optional = true,
+        opts = {
+            defaults = {
+                ['<leader>s'] = { name = '+Search (Flash)' },
+            },
+        },
+    }
+}
