2fe45f232f2665534c54b4fa894a8bc15786d2ec
Andrés Silva <asilvam133@gmail.com>
2023-12-18 10:02:52 -0500

    Add Telescope buffers picker keymap

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -30,6 +30,7 @@ return {
             { '<leader>fd', builtin.diagnostics, mode = 'n', desc = 'Find diagnostics' },
             { '<leader>fs', builtin.grep_string, mode = 'n', desc = 'Grep under cursor' },
             { '<leader>fh', builtin.help_tags, mode = 'n', desc = 'Git: List help tags' },
+            { '<leader>fb', builtin.buffers, mode = 'n', desc = 'Git: List help tags' },
             { '<leader>gg', builtin.git_status, mode = 'n', desc = 'Git: List current changes' },
         }
     end,
