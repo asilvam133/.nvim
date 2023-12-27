e66b188ba9f0876c10349f7dd6abe8b327da237e
Andrés Silva <asilvam133@gmail.com>
2023-12-27 09:36:36 -0500

    Add Neorg

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -72,6 +72,7 @@ return {
                 { name = 'nvim_lsp' },
                 { name = 'luasnip' },
                 { name = 'path' },
+                { name = 'neorg' },
             }, {
                 { name = 'buffer' },
             }),
