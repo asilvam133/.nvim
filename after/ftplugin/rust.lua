2ce7738230e570169d607ee60de8a96ccf2ddf51
Andrés Silva <asilvam133@gmail.com>
2024-01-06 06:08:13 -0500

    Change debuggables keymap to take DAP continue

--- after/ftplugin/rust.lua
+++ after/ftplugin/rust.lua
@@ -1,7 +1,8 @@
 local bufnr = vim.api.nvim_get_current_buf()
 
 --debuggables
-vim.keymap.set('n', '<leader>cd', function()
+vim.keymap.set('n', '<leader>d<space>', '<Nop>')
+vim.keymap.set('n', '<leader>d<space>', function()
     vim.cmd.RustLsp({
         'debuggables',
         'last' --[[ optional ]],
