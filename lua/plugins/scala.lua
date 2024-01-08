c4123eac68b75e93feeb149dc4070efdbae335c7
Andrés Silva <asilvam133@gmail.com>
2024-01-08 06:48:51 -0500

    Remove duplicate CMP definition

--- lua/plugins/scala.lua
+++ lua/plugins/scala.lua
@@ -1,11 +1,5 @@
 return {
     {
-        'hrsh7th/nvim-cmp',
-        dependencies = {
-            { 'hrsh7th/cmp-nvim-lsp' },
-        },
-    },
-    {
         'scalameta/nvim-metals',
         ft = { 'scala', 'sbt' },
         dependencies = { 'nvim-lua/plenary.nvim', 'mfussenegger/nvim-dap' },
