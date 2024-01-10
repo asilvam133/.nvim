c40e212cd862990e549a83294e760c7d3bf1ed0a
Andrés Silva <asilvam133@gmail.com>
2024-01-10 03:27:10 -0500

    Improve Telescope mappings and delete and forget

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -43,49 +43,36 @@ return {
             local builtin = require('telescope.builtin')
 
             return {
+                { '<leader>/', builtin.resume, mode = 'n', desc = 'Resume search' },
+                { '<C-p>', builtin.git_status, mode = 'n', desc = 'Git changes' },
+                { '<leader>vh', builtin.help_tags, mode = 'n', desc = 'Help tags' },
+                { '<leader>vb', builtin.buffers, mode = 'n', desc = 'Buffers' },
+                { '<leader>pw', builtin.grep_string, mode = 'n', desc = 'Project word under cursor' },
                 {
                     '<leader>pf',
                     function()
                         builtin.find_files()
                     end,
                     mode = 'n',
-                    desc = 'Find files',
+                    desc = 'Project files',
                 },
                 {
-                    '<leader>ph',
+                    '<leader>ps',
                     function()
-                        builtin.find_files({ hidden = true })
+                        builtin.grep_string({ search = vim.fn.input('Restrict by = ') })
                     end,
                     mode = 'n',
-                    desc = 'Find files (hidden)',
+                    desc = 'Project search',
                 },
-                { '<C-p>', builtin.git_status, mode = 'n', desc = 'List git changes' },
-                { '<leader><space>', builtin.live_grep, mode = 'n', desc = 'Find using grep' },
-                { '<leader>/', builtin.resume, mode = 'n', desc = 'Resume search' },
                 {
-                    '<leader>f/',
-                    function()
-                        builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown({
-                            previewer = false,
-                        }))
-                    end,
-                    mode = 'n',
-                    desc = 'Grep current buffer',
-                },
-                { '<leader>fd', builtin.diagnostics, mode = 'n', desc = 'Find diagnostics' },
-                { '<leader>fw', builtin.grep_string, mode = 'n', desc = 'Find word under cursor' },
-                {
-                    '<leader>fW',
+                    '<leader>pW',
                     function()
                         local word = vim.fn.expand('<cWORD>')
                         builtin.grep_string({ search = word })
                     end,
                     mode = 'n',
-                    desc = 'Find WORD under cursor',
+                    desc = 'Project WORD under cursor',
                 },
-                { '<leader>fh', builtin.help_tags, mode = 'n', desc = 'List help tags' },
-                { '<leader>fb', builtin.buffers, mode = 'n', desc = 'List buffers' },
-                { '<leader>fk', builtin.keymaps, mode = 'n', desc = 'List keymaps' },
             }
         end,
     },
