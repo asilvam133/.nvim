acd01dbd382018548816c22ecdad44b5fdfc9f7e
Andrés Silva <asilvam133@gmail.com>
2024-01-25 06:54:17 -0500

    I don't use this

--- lua/plugins/indent-blankline.lua
+++
@@ -1,8 +0,0 @@
-return {
-    'lukas-reineke/indent-blankline.nvim',
-    main = 'ibl',
-    opts = {},
-    keys = {
-        { '<leader>ui', '<cmd>IBLToggle<cr>', mode = 'n', desc = 'Toggle indent guides' },
-    },
-}
