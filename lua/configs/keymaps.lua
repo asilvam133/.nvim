6283c23f7283919f03ab96368153f82d277f206a
Andrés Silva <asilvam133@gmail.com>
2024-01-25 06:54:17 -0500

    I don't use this

--- lua/configs/keymaps.lua
+++ lua/configs/keymaps.lua
@@ -20,14 +20,6 @@ vim.keymap.set('n', 'zv', '<cmd>set cursorline!<cr>', { desc = 'Show cursor line
 -- Next occurrence, center view and show cursor line
 vim.keymap.set('n', 'n', 'nzz<cmd>set cursorline<cr>', { desc = 'Next occurence and center view' })
 
--- Toggle wrap
-local toggle_wrap = function()
-    vim.cmd('set wrap!')
-    vim.cmd('set nolist!')
-    vim.cmd('set linebreak!')
-end
-vim.keymap.set('n', '<leader>uw', toggle_wrap, { desc = 'Toggle wrap' })
-
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
