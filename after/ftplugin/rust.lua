a51e3361829bc15a64063540dd24fac7abf60f49
Andrés Silva <asilvam133@gmail.com>
2024-01-29 08:49:10 -0500

    Always list options for Rust debuggables

--- after/ftplugin/rust.lua
+++ after/ftplugin/rust.lua
@@ -5,6 +5,5 @@ vim.keymap.set('n', '<leader>d<space>', '<Nop>')
 vim.keymap.set('n', '<leader>d<space>', function()
     vim.cmd.RustLsp({
         'debuggables',
-        'last' --[[ optional ]],
     })
 end, { silent = true, buffer = bufnr, desc = 'Rust: Debuggables' })
