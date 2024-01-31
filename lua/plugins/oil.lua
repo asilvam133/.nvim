53ca0dfb831694d5c0fdf29471a27aede572e220
Andrés Silva <asilvam133@gmail.com>
2024-01-31 04:55:14 -0500

    Remap some Oil mappings
    They conflict with Harpoon

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -2,7 +2,14 @@ return {
     'stevearc/oil.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     lazy = false,
-    opts = {},
+    opts = {
+        keymaps = {
+            ['<C-h>'] = false,
+            ['<C-t>'] = false,
+            ['<C-s>'] = false,
+            ['<C-v>'] = 'actions.select_vsplit',
+        },
+    },
     keys = {
         {
             '<leader>pv',
