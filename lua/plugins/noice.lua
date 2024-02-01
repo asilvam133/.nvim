19eb160f802a02da4cd400e3f41d378ce227bc65
Andrés Silva <asilvam133@gmail.com>
2024-02-01 13:31:55 -0500

    Bring back Noice + Dressing
    Dressing.nvim solves the input and select behaviour

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,51 @@
+return {
+    'folke/noice.nvim',
+    event = 'VeryLazy',
+    dependencies = {
+        'MunifTanjim/nui.nvim',
+        {
+            'rcarriga/nvim-notify',
+            opts = {
+                background_colour = '#000000',
+            },
+        },
+    },
+    opts = {
+        lsp = {
+            -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+            override = {
+                ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+                ['vim.lsp.util.stylize_markdown'] = true,
+                ['cmp.entry.get_documentation'] = true,
+            },
+        },
+        -- you can enable a preset for easier configuration
+        presets = {
+            bottom_search = true,
+            command_palette = true,
+            long_message_to_split = true,
+            inc_rename = false,
+            lsp_doc_border = false,
+        },
+        routes = {
+            {
+                filter = {
+                    event = 'msg_show',
+                    kind = '',
+                    find = 'written',
+                },
+                opts = { skip = true },
+            },
+        },
+    },
+    keys = {
+        {
+            '<leader>nd',
+            function()
+                require('noice').cmd('dismiss')
+            end,
+            mode = 'n',
+            desc = 'Noice dismiss',
+        },
+    },
+}
