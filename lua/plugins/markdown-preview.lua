ec88cca17de456a115da3e61bd92c8056a325178
Andrés Silva <asilvam133@gmail.com>
2024-02-05 03:15:28 -0500

    Add Markdown-preview

---
+++ lua/plugins/markdown-preview.lua
@@ -0,0 +1,18 @@
+return {
+    'iamcco/markdown-preview.nvim',
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    build = function()
+        vim.fn['mkdp#util#install']()
+    end,
+    keys = {
+        {
+            '<leader>mp',
+            ft = 'markdown',
+            '<cmd>MarkdownPreviewToggle<cr>',
+            desc = 'Markdown preview',
+        },
+    },
+    config = function()
+        vim.cmd([[do FileType]])
+    end,
+}
