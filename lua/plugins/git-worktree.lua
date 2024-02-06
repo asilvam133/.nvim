5b29916614e2272a06be47809bc2bccd76abe1f1
Andrés Silva <asilvam133@gmail.com>
2024-02-06 17:26:20 -0500

    Telescope dropdown theme for git-worktree

--- lua/plugins/git-worktree.lua
+++ lua/plugins/git-worktree.lua
@@ -2,17 +2,26 @@ return {
     'ThePrimeagen/git-worktree.nvim',
     keys = function()
         require('telescope').load_extension('git_worktree')
+        local themes = require('telescope.themes')
 
         return {
             {
                 '<leader>ga',
-                require('telescope').extensions.git_worktree.create_git_worktree,
+                function()
+                    require('telescope').extensions.git_worktree.create_git_worktree(
+                        themes.get_dropdown()
+                    )
+                end,
                 mode = 'n',
                 desc = 'Git worktree add',
             },
             {
                 '<leader>gs',
-                require('telescope').extensions.git_worktree.git_worktrees,
+                function()
+                    require('telescope').extensions.git_worktree.git_worktrees(
+                        themes.get_dropdown()
+                    )
+                end,
                 mode = 'n',
                 desc = 'Git worktree switch (C-d to delete)',
             },
