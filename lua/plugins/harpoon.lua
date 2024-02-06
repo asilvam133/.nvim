c9d91ed53491ff44718c6ce9c07be5d59f78ac17
Andrés Silva <asilvam133@gmail.com>
2024-02-06 14:51:15 -0500

    Refactor mappings

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -5,55 +5,57 @@ return {
     config = function()
         require('harpoon'):setup()
     end,
-    keys = {
-        {
-            '<C-e>',
-            function()
-                local harpoon = require('harpoon')
-                harpoon.ui:toggle_quick_menu(harpoon:list())
-            end,
-            mode = 'n',
-            desc = 'Harpoon: Show marks',
-        },
-        {
-            '<leader>a',
-            function()
-                require('harpoon'):list():append()
-            end,
-            mode = 'n',
-            desc = 'Harpoon: Mark file',
-        },
-        {
-            '<C-h>',
-            function()
-                require('harpoon'):list():select(1)
-            end,
-            mode = 'n',
-            desc = 'Harpoon: Go to mark 1',
-        },
-        {
-            '<C-t>',
-            function()
-                require('harpoon'):list():select(2)
-            end,
-            mode = 'n',
-            desc = 'Harpoon: Go to mark 2',
-        },
-        {
-            '<C-n>',
-            function()
-                require('harpoon'):list():select(3)
-            end,
-            mode = 'n',
-            desc = 'Harpoon: Go to mark 3',
-        },
-        {
-            '<C-s>',
-            function()
-                require('harpoon'):list():select(4)
-            end,
-            mode = 'n',
-            desc = 'Harpoon: Go to mark 4',
-        },
-    },
+    keys = function()
+        local harpoon = require('harpoon')
+        return {
+            {
+                '<C-e>',
+                function()
+                    harpoon.ui:toggle_quick_menu(harpoon:list())
+                end,
+                mode = 'n',
+                desc = 'Harpoon: Show marks',
+            },
+            {
+                '<leader>a',
+                function()
+                    harpoon:list():append()
+                end,
+                mode = 'n',
+                desc = 'Harpoon: Mark file',
+            },
+            {
+                '<C-h>',
+                function()
+                    harpoon:list():select(1)
+                end,
+                mode = 'n',
+                desc = 'Harpoon: Go to mark 1',
+            },
+            {
+                '<C-t>',
+                function()
+                    harpoon:list():select(2)
+                end,
+                mode = 'n',
+                desc = 'Harpoon: Go to mark 2',
+            },
+            {
+                '<C-n>',
+                function()
+                    harpoon:list():select(3)
+                end,
+                mode = 'n',
+                desc = 'Harpoon: Go to mark 3',
+            },
+            {
+                '<C-s>',
+                function()
+                    harpoon:list():select(4)
+                end,
+                mode = 'n',
+                desc = 'Harpoon: Go to mark 4',
+            },
+        }
+    end,
 }
