c3fb390d1fe8b676d8571f26c8b40b218d869bd4
Andrés Silva <asilvam133@gmail.com>
2024-02-13 12:33:11 -0500

    Support for HTML

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -10,6 +10,7 @@ return {
             require('nvim-treesitter.configs').setup({
                 ensure_installed = {
                     'bash',
+                    'html',
                     'lua',
                     'markdown',
                     'markdown_inline',
