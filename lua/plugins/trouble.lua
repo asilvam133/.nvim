6f8e2acee8d52681fb9fd7de99d5ceb174a8fcf6
Andrés Silva <asilvam133@gmail.com>
2024-02-15 17:21:28 -0500

    Set easier Trouble keymaps

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -4,7 +4,7 @@ return {
     opts = {},
     keys = {
         {
-            '<leader>tq',
+            '<leader>q',
             function()
                 require('trouble').toggle('quickfix')
             end,
@@ -12,7 +12,7 @@ return {
             desc = 'Trouble: Quickfix',
         },
         {
-            '<leader>td',
+            '<leader>k',
             function()
                 require('trouble').toggle('document_diagnostics')
             end,
@@ -20,7 +20,7 @@ return {
             desc = 'Trouble: Diagnostics (Document)',
         },
         {
-            '<leader>tD',
+            '<leader>K',
             function()
                 require('trouble').toggle('workspace_diagnostics')
             end,
