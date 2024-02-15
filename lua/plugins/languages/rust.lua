834a963e7025b18ebdbe7d7b422c5a1b8df6cf64
Andrés Silva <asilvam133@gmail.com>
2024-02-15 18:02:42 -0500

    Add breadcrumbs using nvim-navic

--- lua/plugins/languages/rust.lua
+++ lua/plugins/languages/rust.lua
@@ -1,7 +1,18 @@
 return {
-    {
-        'mrcjkb/rustaceanvim',
-        version = '^3', -- Recommended
-        ft = { 'rust' },
-    },
+    'mrcjkb/rustaceanvim',
+    version = '^3', -- Recommended
+    ft = { 'rust' },
+    config = function()
+        local navic = require('nvim-navic')
+        local on_attach = function(client, bufnr)
+            if client.server_capabilities.documentSymbolProvider then
+                navic.attach(client, bufnr)
+            end
+        end
+        vim.g.rustaceanvim = {
+            server = {
+                on_attach = on_attach,
+            },
+        }
+    end,
 }
