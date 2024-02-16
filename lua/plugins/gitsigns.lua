662850a727c175ff6e2b982064036e3cfb62199d
Andrés Silva <asilvam133@gmail.com>
2024-02-16 05:26:01 -0500

    Remove unused mappings

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -12,19 +12,6 @@ return {
             changedelete = { text = '~' },
         },
         on_attach = function(bufnr)
-            vim.keymap.set(
-                'n',
-                '<leader>gp',
-                require('gitsigns').preview_hunk,
-                { buffer = bufnr, desc = 'Git preview hunk' }
-            )
-            vim.keymap.set(
-                'n',
-                '<leader>gh',
-                require('gitsigns').stage_hunk,
-                { buffer = bufnr, desc = 'Git stage hunk' }
-            )
-
             -- don't override the built-in and fugitive keymaps
             local gs = package.loaded.gitsigns
             vim.keymap.set({ 'n', 'v' }, ']c', function()
