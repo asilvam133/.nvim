d1bc29c635235c06ca4bf38e3c58964b6d1f2757
Andrés Silva <asilvam133@gmail.com>
2024-02-16 08:00:09 -0500

    Drop Navic. Didn't like it

--- lua/plugins/languages/scala.lua
+++ lua/plugins/languages/scala.lua
@@ -1,54 +1,47 @@
 return {
-    {
-        'scalameta/nvim-metals',
-        ft = { 'scala', 'sbt' },
-        dependencies = { 'nvim-lua/plenary.nvim', 'mfussenegger/nvim-dap' },
-        config = function()
-            local metals_config = require('metals').bare_config()
-            metals_config.init_options.statusBarProvider = 'on'
-            metals_config.settings = {
-                showImplicitArguments = true,
-                excludedPackages = { 'akka.actor.typed.javadsl', 'com.github.swagger.akka.javadsl' },
-            }
-            metals_config.capabilities = require('cmp_nvim_lsp').default_capabilities()
-            metals_config.on_attach = function(client, bufnr)
-                require('metals').setup_dap()
+    'scalameta/nvim-metals',
+    ft = { 'scala', 'sbt' },
+    dependencies = { 'nvim-lua/plenary.nvim', 'mfussenegger/nvim-dap' },
+    config = function()
+        local metals_config = require('metals').bare_config()
+        metals_config.init_options.statusBarProvider = 'on'
+        metals_config.settings = {
+            showImplicitArguments = true,
+            excludedPackages = { 'akka.actor.typed.javadsl', 'com.github.swagger.akka.javadsl' },
+        }
+        metals_config.capabilities = require('cmp_nvim_lsp').default_capabilities()
+        metals_config.on_attach = function(_, _)
+            require('metals').setup_dap()
+        end
 
-                local navic = require('nvim-navic')
-                if client.server_capabilities.documentSymbolProvider then
-                    navic.attach(client, bufnr)
-                end
-            end
+        local nvim_metals_group = vim.api.nvim_create_augroup('nvim-metals', { clear = true })
+        vim.api.nvim_create_autocmd('FileType', {
+            pattern = { 'scala', 'sbt', 'java' },
+            callback = function()
+                require('metals').initialize_or_attach(metals_config)
+            end,
+            group = nvim_metals_group,
+        })
 
-            local nvim_metals_group = vim.api.nvim_create_augroup('nvim-metals', { clear = true })
-            vim.api.nvim_create_autocmd('FileType', {
-                pattern = { 'scala', 'sbt', 'java' },
-                callback = function()
-                    require('metals').initialize_or_attach(metals_config)
-                end,
-                group = nvim_metals_group,
-            })
-
-            -- Debug settings
-            local dap = require('dap')
-            dap.configurations.scala = {
-                {
-                    type = 'scala',
-                    request = 'launch',
-                    name = 'Run or test',
-                    metals = {
-                        runType = 'runOrTestFile',
-                    },
+        -- Debug settings
+        local dap = require('dap')
+        dap.configurations.scala = {
+            {
+                type = 'scala',
+                request = 'launch',
+                name = 'Run or test',
+                metals = {
+                    runType = 'runOrTestFile',
                 },
-                {
-                    type = 'scala',
-                    request = 'launch',
-                    name = 'Test build target',
-                    metals = {
-                        runType = 'testTarget',
-                    },
+            },
+            {
+                type = 'scala',
+                request = 'launch',
+                name = 'Test build target',
+                metals = {
+                    runType = 'testTarget',
                 },
-            }
-        end,
-    },
+            },
+        }
+    end,
 }
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -5,53 +5,38 @@ return {
         'nvim-tree/nvim-web-devicons',
         'meuter/lualine-so-fancy.nvim',
     },
-    opts = function()
-        local navic = require('nvim-navic')
-        return {
-            options = {
-                component_separators = { left = '│', right = '│' },
-                section_separators = { left = '', right = '' },
-                globalstatus = true,
-                refresh = {
-                    statusline = 100,
-                },
+    opts = {
+        options = {
+            component_separators = { left = '│', right = '│' },
+            section_separators = { left = '', right = '' },
+            globalstatus = true,
+            refresh = {
+                statusline = 100,
             },
-            sections = {
-                lualine_a = {
-                    { 'fancy_mode' },
-                },
-                lualine_b = {
-                    { 'fancy_branch' },
-                    { 'fancy_diff' },
-                },
-                lualine_c = {
-                    { 'filename' },
-                },
-                lualine_x = {
-                    { 'fancy_macro' },
-                    { 'fancy_diagnostics' },
-                    { 'fancy_searchcount' },
-                    { 'fancy_location' },
-                },
-                lualine_y = {
-                    { 'fancy_filetype', ts_icon = '' },
-                },
-                lualine_z = {
-                    { 'fancy_lsp_servers' },
-                },
+        },
+        sections = {
+            lualine_a = {
+                { 'fancy_mode' },
             },
-            winbar = {
-                lualine_c = {
-                    {
-                        function()
-                            return navic.get_location()
-                        end,
-                        cond = function()
-                            return navic.is_available()
-                        end,
-                    },
-                },
+            lualine_b = {
+                { 'fancy_branch' },
+                { 'fancy_diff' },
             },
-        }
-    end,
+            lualine_c = {
+                { 'filename' },
+            },
+            lualine_x = {
+                { 'fancy_macro' },
+                { 'fancy_diagnostics' },
+                { 'fancy_searchcount' },
+                { 'fancy_location' },
+            },
+            lualine_y = {
+                { 'fancy_filetype', ts_icon = '' },
+            },
+            lualine_z = {
+                { 'fancy_lsp_servers' },
+            },
+        },
+    },
 }
