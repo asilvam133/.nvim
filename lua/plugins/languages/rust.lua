a43925d3e9c08018bbaa52b8e6df4d6b9aed02b0
Andrés Silva <asilvam133@gmail.com>
2024-02-17 04:40:42 -0500

    Add NVIM 0.10 inlay hints support

--- lua/plugins/languages/rust.lua
+++ lua/plugins/languages/rust.lua
@@ -2,4 +2,13 @@ return {
     'mrcjkb/rustaceanvim',
     version = '^3', -- Recommended
     ft = { 'rust' },
+    config = function()
+        local on_attach = require('utils.lsp').on_attach
+
+        vim.g.rustaceanvim = {
+            server = {
+                on_attach = on_attach,
+            },
+        }
+    end,
 }
