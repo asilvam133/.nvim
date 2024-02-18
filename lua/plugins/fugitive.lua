55c91f665794d115447f474fa156b8fd5b74043d
Andrés Silva <asilvam133@gmail.com>
2024-02-18 03:32:25 -0500

    Remove event because of lazy keymap loading
    Change <leader>go keymap to open Fugitive UI in new tab

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,9 +1,8 @@
 return {
     {
         'tpope/vim-fugitive',
-        event = 'BufWinEnter',
         keys = {
-            { '<leader>go', '<cmd>Git<cr>', mode = 'n', desc = 'Git open' },
+            { '<leader>go', '<cmd>tab Git<cr>', mode = 'n', desc = 'Git open' },
             { '<leader>gp', '<cmd>Git pull<cr>', mode = 'n', desc = 'Git pull' },
             { '<leader>gP', '<cmd>Git push --force<cr>', mode = 'n', desc = 'Git push (force)' },
         },
