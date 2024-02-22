4cb1b95f90e987307376822b77ea1f11e2bc95f1
Andrés Silva <asilvam133@gmail.com>
2024-02-22 04:09:35 -0500

    Add nvim-lua CMP and crates sources

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -7,6 +7,7 @@ return {
         'hrsh7th/cmp-buffer',
         'hrsh7th/cmp-path',
         'hrsh7th/cmp-cmdline',
+        'hrsh7th/cmp-nvim-lua',
         'saadparwaiz1/cmp_luasnip',
         'hrsh7th/cmp-nvim-lsp-signature-help',
         'hrsh7th/cmp-nvim-lsp',
@@ -73,9 +74,11 @@ return {
             sources = cmp.config.sources({
                 -- nvim_lsp unknown source in CmpStatus because it is lazy loaded on InsertEnter
                 { name = 'nvim_lsp' },
+                { name = 'nvim_lua' },
                 { name = 'nvim_lsp_signature_help' },
                 { name = 'luasnip' },
                 { name = 'path' },
+                { name = 'crates' },
             }, {
                 { name = 'buffer' },
             }),
