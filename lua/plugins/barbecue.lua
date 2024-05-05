af54fd6cb54a3f91b461f8b013c5e82c8a2c0d0c
Andrés Silva <asilvam133@gmail.com>
2024-05-05 18:54:15 -0500

    feat: disable barbecue for now

--- lua/plugins/barbecue.lua
+++ lua/plugins/barbecue.lua
@@ -3,6 +3,7 @@ return {
     name = 'barbecue',
     version = '*',
     event = 'BufRead',
+    enabled = false,
     dependencies = {
         'SmiteshP/nvim-navic',
         'nvim-tree/nvim-web-devicons',
