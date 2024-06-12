f25fa9e755818b280522203cce44489e3a7e8770
Andrés Silva <asilvam133@gmail.com>
2024-06-12 17:37:32 -0500

    feat: bring back italics on comments and disable bk on tabs

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -9,6 +9,16 @@ return {
                 italic = false,
                 transparency = true,
             },
+            highlight_groups = {
+                ['@function.builtin.lua'] = { italic = false },
+                ['@lsp.type.comment'] = { italic = true },
+                ['@lsp.typemod.function.defaultLibrary.lua'] = { italic = false },
+                Comment = { italic = true },
+                DiagnosticUnnecessary = { italic = false },
+                TabLine = { bg = 'none' },
+                TabLineSel = { bg = 'none' },
+                TabLineFill = { bg = 'none' },
+            },
         },
         config = function(_, opts)
             require('rose-pine').setup(opts)
