b3cce83c15d4069405648b8e0c93b5edd457b788
Andres Silva <asilvam133@gmail.com>
2025-01-24 09:53:07 -0500

    feat: Set Scala to have 2 spaces of indentation

--- after/ftplugin/scala.lua
+++ after/ftplugin/scala.lua
@@ -1,4 +1,6 @@
 local bufnr = vim.api.nvim_get_current_buf()
+vim.o.shiftwidth = 2
+vim.o.tabstop = 2
 
 vim.keymap.set('n', '<leader>cs', function()
     require('telescope').extensions.metals.commands()
