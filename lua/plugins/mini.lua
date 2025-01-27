edd9008e76b3d9031cfe4b51a5cef72e06fffa65
Andres Silva <asilvam133@gmail.com>
2025-01-27 11:23:16 -0500

    feat: Add functions textobjects with `aF` and `iF`

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -4,7 +4,14 @@ return {
     lazy = false,
     config = function()
         -- text editing
-        require('mini.ai').setup()
+        require('mini.ai').setup({
+            custom_textobjects = {
+                F = require('mini.ai').gen_spec.treesitter({
+                    a = '@function.outer',
+                    i = '@function.inner',
+                }),
+            },
+        })
         require('mini.surround').setup()
         require('mini.splitjoin').setup()
 
