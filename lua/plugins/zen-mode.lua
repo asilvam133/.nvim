9c0f1d287b6febcc7af8f9fda8cb1d9080a3bc84
Andres Silva <asilvam133@gmail.com>
2025-03-25 16:27:58 -0500

    feat: Increase zen-mode's window width a bit

--- lua/plugins/zen-mode.lua
+++ lua/plugins/zen-mode.lua
@@ -1,7 +1,12 @@
 return {
     'folke/zen-mode.nvim',
     lazy = false,
+    opts = {
+        window = {
+            width = 150,
+        },
+    },
     keys = {
         { '<leader>uz', ':ZenMode<cr>', mode = 'n', desc = 'Toggle Zen Mode' },
-    }
+    },
 }
