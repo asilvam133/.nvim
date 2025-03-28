4631b975b07ac3bf2fbfee7467d4765c1b07f10e
Andres Silva <asilvam133@gmail.com>
2025-03-28 16:55:40 -0500

    feat: Restore default zen mode window width

--- lua/plugins/zen-mode.lua
+++ lua/plugins/zen-mode.lua
@@ -1,11 +1,6 @@
 return {
     'folke/zen-mode.nvim',
     lazy = false,
-    opts = {
-        window = {
-            width = 150,
-        },
-    },
     keys = {
         { '<leader>uz', ':ZenMode<cr>', mode = 'n', desc = 'Toggle Zen Mode' },
     },
