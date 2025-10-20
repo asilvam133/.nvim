8cc39d984f92ddbd2733244b6335501ba350145a
Andres Silva <asilvam133@gmail.com>
2025-10-20 08:29:25 -0500

    feat: Move lualine opts to the opts property

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,15 +1,9 @@
 return {
     'nvim-lualine/lualine.nvim',
-    opts = function()
-        vim.o.laststatus = vim.g.lualine_laststatus
-
-        local opts = {
-            sections = {
-                lualine_b = {},
-                lualine_c = { LazyVim.lualine.pretty_path({ length = 10 }) },
-            },
-        }
-
-        return opts
-    end,
+    opts = {
+        sections = {
+            lualine_b = {},
+            lualine_c = { LazyVim.lualine.pretty_path({ length = 10 }) },
+        },
+    },
 }
