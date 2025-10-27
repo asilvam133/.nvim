42e15db74558a5bfd776a93ab4ed8f71d22d827a
Andres Silva <asilvam133@gmail.com>
2025-10-27 18:09:29 -0500

    feat: Add no-neck-pain in addition to Zen mode

---
+++ lua/plugins/no-neck-pain.lua
@@ -0,0 +1,10 @@
+return {
+    'shortcuts/no-neck-pain.nvim',
+    version = '*',
+    opts = {
+        width = 150,
+    },
+    keys = {
+        { '<leader>U', '<cmd>NoNeckPain<cr>', desc = 'Toggle no-neck-pain' },
+    },
+}
