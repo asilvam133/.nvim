1c0dc54b514f231af22d3cc23d00706a7f26ad76
Andres Silva <asilvam133@gmail.com>
2025-12-16 09:55:05 -0500

    feat: DAP to open when init, and avoid closing after terminating

--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -4,12 +4,8 @@ return {
         local dap = require('dap')
         local dapui = require('dapui')
         dapui.setup(opts)
-        dap.listeners.after.event_initialized['dapui_config'] = function()
-            dapui.open({})
-        end
-        dap.listeners.before.event_terminated['dapui_config'] = function()
-        end
-        dap.listeners.before.event_exited['dapui_config'] = function()
-        end
+        dap.listeners.after.event_initialized['dapui_config'] = function() end
+        dap.listeners.before.event_terminated['dapui_config'] = function() end
+        dap.listeners.before.event_exited['dapui_config'] = function() end
     end,
 }
