1fc7620cb6e1c58881d87a32b5dfee5a4c3a0b25
Andres Silva <asilvam133@gmail.com>
2026-07-01 09:20:57 -0500

    feat(shell): Support Fish

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,10 +2,12 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.autoformat = false
+vim.g.editorconfig = true
 
 local opt = vim.opt
 opt.clipboard = ''
 opt.showcmd = true
+opt.shell = "/bin/zsh"
 
 opt.shiftwidth = 4 -- Size of an indent
 opt.tabstop = 4 -- Number of spaces tabs count for
